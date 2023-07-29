50bcba74add4d955c27783efaaee48b1a87237dc
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-07-29 10:00:10 +0530

    Excersice

---
+++ cal.py
@@ -0,0 +1,30 @@
+a = 15
+b = 7
+c = a+b
+print("The addition of", a, "+", b, " is :", c)
+c = a-b
+print("The Subtraction of", a, "-", b, " is :", c)
+c = a*b
+print("The Multiplication of", a, "*", b, " is :", c)
+c = a/b
+print("The Division of", a, "/", b, " is :", c)
+c = a % b
+print("The Modulo of", a, "%", b, " is :", c)
+c = a//b
+print("The Division of two number using float division operator is  :", c)
+c = a**b
+print("The Square of", a, "^", b, " is :", c)
+a = "1"
+b = "2"
+# Typecasting
+print(int(a)+int(b))
+# Implicit type casting
+a = 1.9
+b = 8
+print(a+b)
+# user input
+a = input("Enter your name : ")
+print("My name is "+a)
+a = int(input("Enter first number : "))  # Typecasting
+b = str(input("Enter second number : "))
+print(a+b)
