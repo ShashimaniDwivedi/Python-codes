1654dd9b7b3c701783bc11d561a839c3978f1146
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-11-08 21:26:02 +0530

    AccessModifier

--- AccessModifier.py
+++ AccessModifier.py
@@ -1,8 +1,28 @@
-class Employee:
-  def __init__(self,name):
-    self._name = name
-    
-a=Employee("Harry")
-# print(a.__name)  throw Error
-print(a._Employee__name) 
-print(a._name) 
+# class ABC:
+#     def __int__(self):
+#         self.public_attribute = None # public attribute
+#
+#     def public_function(self):
+#         pass
+
+# Private Attribute (2 __)
+# class ABC:
+#     def __init__(self):
+#         self.__private_attribute = None  # private attribute
+#
+#     def __private_function(self):
+#         pass
+#
+#
+# obj1 = ABC()
+# print(obj1.private_attribute)  # Through error
+
+# Protected Attribute (1 _ )
+class ABC:
+    def __int__(self):
+        self._protected_attribute = None # protected attribute
+
+    def _protected_function(self):
+        pass
+
+
