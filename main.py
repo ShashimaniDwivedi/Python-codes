c587fe2b8c77d75180214074372230a78fdd9796
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2023-11-11 08:25:57 +0530

    Python

--- main.py
+++ main.py
@@ -1,66 +1,79 @@
-import pandas
-import hashlib
-"""
-Multiple line comment
-our first python program
-"""
-# Single line comment
+def add(a, b):
+    print(a + b)
 
-print("Hello world hey,4,5,6 \"I am a good boy\"\nBye", end="\n")
-print("Hey", 4, 5, sep="~")
-a = 1234567889
-print(a)
-b = "Manas"
-print("The type of b is : ", type(b))
-c = True
-d = None
-print(type(c), type(d))
-e = complex(3, 4)
-print(e)
-list = [3, 4, 6, 2, ["Banana", "Mango"], [2.3, 4.5]]
-print(list)
-tuple = (("Code", "Harry"), ("RAM", "SITA"), 3, 4, 5)
-print(tuple)
-dict = {"Name": "Shashi", "Age": 18, "Height": "6Ft"}
-# print(dict)
-apple = """He said,
-Hi Harry
-Hey I am good
-"I want to eat an apple\""""
-name = "apple"
-print(apple)
-print(name[0])
-print("Let's Use For Loop")
-for character in apple:
-    print(character)
-names = "Harry,Manas"
-print(names[0:5], len(names))  # to find length of string
-# names[:] give full name                 negative Slicing
-# behind the scene names[0:len(names)-3]
-# behind the scene names[len(names)-3:len(names)-1]
-# including 1 but not 3
-print(names[1:3], names[:5], names[:], names[5:], names[0:-3], names[-3:-1])
-nm = "Harry*****Raj Paul"
-# new copy of string strings are immutable
-print(nm.upper(), nm.lower(), nm.strip(),
-      nm.rstrip("*"), nm.replace("Harry", "Manas"), nm.split(" "))
-blogHeading = "introduction to python"
-print(blogHeading.capitalize())
-str = "Welcome to the Console Python interpreter!\n"
-print(len(str))
-print(len(str.center(50)))
-print(str.center(50))
-print(nm.count("Harry"))
-print(str.endswith("!"))  # give Boolean value
-print(str.endswith("to", 4, 10))
-str1 = "My name is manas!"
-print(str1.find("is"))
-# print(str1.index("isjv"))  # Throw exception Value error
-str2 = "World Health Organization"
-print(str.isalnum())
-print(str.isalpha())
-print(str.isprintable())
-print(str.isspace())
-print(str2.istitle())
-print(str2.swapcase())
-print(str2.title())
+
+# Default argument come after non-default argument
+def squa(a, b=2):
+    print(a ** b)
+
+
+# Positional Argument
+add(2, 3)
+
+# Keyword argument
+add(b=5, a=3)
+
+# Default Argument
+squa(5)
+
+
+# Arbitrary Argument 1. (Variable length)
+# Take input as "TUPLE"
+def addAllNum(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
+
+
+res = addAllNum(1, 2, 3, 4, 5)
+print("Sum of all numbers : ", res)
+
+
+# 2.Keyword Argument
+def studentInfo(**kwargs):
+    for i, j in kwargs.items():
+        print(i, "is", j)
+
+
+studentInfo(Name="Manas", age=19, city="Lucknow")
+
+
+def outer_func():
+    x = 1
+
+    def inner_func():
+        y = 2
+        result = x + y
+        return result
+
+    return inner_func()
+
+
+output = outer_func()
+print(output)
+
+
+# Pass by Value(Immutable Objects)
+def sum1(a, b):
+    a += 1
+    b += 1
+    print(a, b)
+
+
+a = 2
+b = 3
+print("Inside func : ", end="")
+sum1(a, b)
+print("Outside func : ", a, b)
+
+
+# Pass by Reference(Mutable Objects)
+def modify_list(lst):
+    lst.append(4)
+    print("Inside list", lst)
+
+
+lst = [1, 2, 3]
+modify_list(lst)
+print("Outside func : ", lst)
